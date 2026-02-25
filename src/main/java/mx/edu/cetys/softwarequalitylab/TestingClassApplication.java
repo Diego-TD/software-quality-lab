fb0621406a085366b55bda17a388c9df1d95879f
Diego Torres <diegotorresdurante@gmail.com>
2026-02-24 20:21:32 -0800

    init project

---
+++ src/main/java/mx/edu/cetys/softwarequalitylab/TestingClassApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.cetys.softwarequalitylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/cetys/softwarequalitylab/TestingClassApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.cetys.softwarequalitylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
